ea53937a238055860654641a99928fbb21213184
hp <hp@DESKTOP-U78EJ0R>
2020-03-09 17:50:11 +0530

    task4

---
+++ nihatask4/src/main/java/Week4/week4/House_Construction.java
@@ -0,0 +1,21 @@
+package Week4.week4;
+
+public class House_Construction {
+	public double cost(String standard_material,String fully_automated,double sq_feet) {
+		double cost=0;
+		if(standard_material=="yes" && fully_automated=="no") {
+			cost=sq_feet*1200;
+		}
+		else if(standard_material=="above" && fully_automated=="no") {
+			cost=sq_feet*1500;
+		}
+		else if(standard_material=="high" && fully_automated=="no") {
+			cost=sq_feet*1800;
+		}
+		else if(standard_material=="high" && fully_automated=="yes") {
+			cost=sq_feet*2500;
+		}
+		return cost;
+	}
+
+}
